3adc67213eb32f74dfa9de675280028ce591ef46
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-09-27 12:56:40 -0500

    improved documentation

--- src/Text/Tabl/Environment.hs
+++ src/Text/Tabl/Environment.hs
@@ -2,7 +2,8 @@ module Text.Tabl.Environment
 ( Environment(..)
 ) where
 
--- Output environment.
+-- | Output environment that declares the way that the table will be
+-- rendered.
 data Environment
   = EnvAscii -- ^ ASCII art suitable for command line
   | EnvLatex -- ^ LaTeX source code
