039d262dd85c1cd82a7726f6ecd74fb4248c231d
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-10-23 23:38:44 +0100

    removed invalid documentation

--- src/Text/Tabl/Util.hs
+++ src/Text/Tabl/Util.hs
@@ -13,11 +13,11 @@ extend :: Int -- ^ expected length
 extend n x xs = xs ++ replicate (n - length xs) x
 
 -- | Insert an element before i-th position, if the i-th Bool is True.
-intersperseOn :: (Monoid a) -- ^ required for mempty
-              => [a]        -- ^ list
-              -> [Bool]     -- ^ insert rules
-              -> a          -- ^ element to insert
-              -> [a]        -- ^ new list
+intersperseOn :: (Monoid a)
+              => [a]    -- ^ list
+              -> [Bool] -- ^ insert rules
+              -> a      -- ^ element to insert
+              -> [a]    -- ^ new list
 intersperseOn xs bs x = init $ concat $ zipWith glue bs (xs ++ [mempty])
   where
     glue True i  = [x, i]
@@ -33,9 +33,9 @@ bool _ y False = y
 
 -- | Create an object by zipping two lists together. The second list is
 -- expected to be one element shorter.
-zipcat :: (Monoid a) -- ^ required for mempty
-       => [a]        -- ^ first list
-       -> [a]        -- ^ second list
-       -> a          -- ^ result
+zipcat :: (Monoid a)
+       => [a] -- ^ first list
+       -> [a] -- ^ second list
+       -> a   -- ^ result
 zipcat xs ys = mconcat $ zipWith mappend xs (mappend ys [mempty])
 
