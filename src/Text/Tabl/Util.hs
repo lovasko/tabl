68f29fd86c60cfb88c6097d966431130bb2efbbf
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-11-20 19:04:50 +0000

    improved docs

--- src/Text/Tabl/Util.hs
+++ src/Text/Tabl/Util.hs
@@ -8,7 +8,7 @@ Maintainer  : Daniel Lovasko <daniel.lovasko@gmail.com>
 Stability   : stable
 Portability : portable
 
-Set of general utilities that are used across the codebase.
+Set of general utilities that are used across the whole project.
 -}
 
 module Text.Tabl.Util
@@ -18,14 +18,16 @@ module Text.Tabl.Util
 , zipcat
 ) where
 
--- | Extend a list to defined length with one repeated element.
+-- | Extend a list to a defined length with one repeated element. This
+-- function assumes that the list is shorter than the provided length.
 extend :: Int -- ^ expected length
        -> a   -- ^ element to pad with
        -> [a] -- ^ original list
        -> [a] -- ^ extended list
 extend n x xs = xs ++ replicate (n - length xs) x
 
--- | Insert an element before i-th position, if the i-th Bool is True.
+-- | Insert an element in front of the i-th position, if the i-th element
+-- is True.
 intersperseOn :: (Monoid a)
               => [a]    -- ^ list
               -> [Bool] -- ^ insert rules
@@ -36,10 +38,10 @@ intersperseOn xs bs x = init $ concat $ zipWith glue bs (xs ++ [mempty])
     glue True i  = [x, i]
     glue False i = [i]
 
--- | Endomorphism on the boolean type.
+-- | Functional implementation of the if/then/else concept.
 bool :: a    -- ^ True option
      -> a    -- ^ False option
-     -> Bool -- ^ bool
+     -> Bool -- ^ condition
      -> a    -- ^ result
 bool x _ True  = x
 bool _ y False = y
