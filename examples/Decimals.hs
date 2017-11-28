8addfd84a704f8be6424af9813b256e62555a7bd
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-11-28 15:51:40 +0000

    adjusted constats to improve the decimals example

--- examples/Decimals.hs
+++ examples/Decimals.hs
@@ -7,9 +7,9 @@ import qualified Data.Text.IO as T
 main :: IO ()
 main = T.putStrLn $ tabl EnvAscii DecorOuter DecorAll aligns cells
   where
-    aligns  = [AlignRight, AlignText "."]
-    cells   = zipWith (\x y -> [x, y]) xs ys
-    xs      = map (T.pack . show) nums
-    ys      = map (T.pack . take 5 . show . tenOver) (map fromIntegral nums)
-    nums    = [1..20] :: [Integer]
-    tenOver = ((10.0 :: Double) /)
+    aligns = [AlignRight, AlignText "."]
+    cells  = zipWith (\x y -> [x, y]) xs ys
+    xs     = map (T.pack . show) nums
+    ys     = map (T.pack . take 5 . show . over) (map fromIntegral nums)
+    nums   = [1..20] :: [Integer]
+    over   = ((100.0 :: Double) /)
