602b12618645b7f23852068da8533194b53e4682
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-11-29 21:39:12 +0000

    simplified number conversion pipeline

--- examples/Decimals.hs
+++ examples/Decimals.hs
@@ -10,6 +10,6 @@ main = T.putStrLn $ tabl EnvAscii DecorOuter DecorAll aligns cells
     aligns = [AlignRight, AlignText "."]
     cells  = zipWith (\x y -> [x, y]) xs ys
     xs     = map (T.pack . show) nums
-    ys     = map (T.pack . take 5 . show . over) (map fromIntegral nums)
+    ys     = map (T.pack . take 5 . show . over . fromIntegral) nums
     nums   = [1..20] :: [Integer]
     over   = ((100.0 :: Double) /)
