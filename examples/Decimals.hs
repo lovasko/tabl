b3bc08759452436d651283b85f9be1e2c45c23dc
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-11-30 22:40:23 +0000

    refactored the decimals example to add command-line arguments

--- examples/Decimals.hs
+++ examples/Decimals.hs
@@ -1,17 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import System.Environment
 import Text.Tabl
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
-main :: IO ()
-main = T.putStrLn $ tabl EnvAscii hdecor vdecor aligns cells
+-- | Create the division table.
+table
+  :: Int    -- ^ number of rows
+  -> T.Text -- ^ resulting table
+table n = tabl EnvAscii hdecor vdecor aligns cells
   where
     hdecor = DecorUnion [DecorIf (\x -> mod x 5 == 0), DecorOuter]
     vdecor = DecorAll
     aligns = [AlignRight, AlignText "."]
     cells  = zipWith (\x y -> [x, y]) xs ys
-    xs     = map (T.pack . show) nums
-    ys     = map (T.pack . take 5 . show . over . fromIntegral) nums
-    nums   = [1..20] :: [Integer]
-    over   = ((100.0 :: Double) /)
+    xs     = map (T.pack . show . round) nums
+    ys     = map (T.pack . take 5 . show . (100.0 /)) nums
+    nums   = take n $ iterate (+1.0) 1.0
+
+main :: IO ()
+main = getArgs >>= (\[n] -> T.putStrLn (table (read n)))
