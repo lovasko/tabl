c873a9ffdab9d335287e2620ef49ad27b92867a1
Daniel Lovasko <daniel.lovasko@gmail.com>
2020-02-26 11:46:49 +0000

    vertical alignment of code

--- src/Text/Tabl/Decoration.hs
+++ src/Text/Tabl/Decoration.hs
@@ -23,13 +23,13 @@ import qualified Data.Text as T
 -- | Decoration style that defines which lines (horizontal or vertical)
 -- will be visible in the resulting table.
 data Decoration
-  = DecorAll                -- ^ all lines
-  | DecorOuter              -- ^ outer lines
-  | DecorOnly [Int]         -- ^ only certain lines
-  | DecorUnion [Decoration] -- ^ union of more decorations
-  | DecorIsect [Decoration] -- ^ intersection of more decorations
+  = DecorAll                          -- ^ all lines
+  | DecorOuter                        -- ^ outer lines
+  | DecorOnly [Int]                   -- ^ only certain lines
+  | DecorUnion [Decoration]           -- ^ union of more decorations
+  | DecorIsect [Decoration]           -- ^ intersection of more decorations
   | DecorIf (Int -> [T.Text] -> Bool) -- ^ based on a predicate result
-  | DecorNegate Decoration  -- ^ opposite of a decoration
+  | DecorNegate Decoration            -- ^ opposite of a decoration
 
 -- | Convert a decoration to a list of presence information.
 presence
@@ -37,13 +37,13 @@ presence
   -> [[T.Text]] -- ^ cells
   -> Decoration -- ^ decoration
   -> [Bool]     -- ^ presence
-presence n _  DecorAll         = replicate n True
-presence n _  DecorOuter       = [True] ++ replicate (n-2) False ++ [True]
-presence n _  (DecorOnly is)   = map (`elem` is) [0..(n-1)]
-presence n cs (DecorUnion ds)  = combine (||) False n cs ds
-presence n cs (DecorIsect ds)  = combine (&&) True  n cs ds
-presence n cs (DecorIf fn)     = zipWith fn [0..(n-1)] cs
-presence n cs (DecorNegate d)  = map not (presence n cs d)
+presence n _  DecorAll        = replicate n True
+presence n _  DecorOuter      = [True] ++ replicate (n-2) False ++ [True]
+presence n _  (DecorOnly is)  = map (`elem` is) [0..(n-1)]
+presence n cs (DecorUnion ds) = combine (||) False n cs ds
+presence n cs (DecorIsect ds) = combine (&&) True  n cs ds
+presence n cs (DecorIf fn)    = zipWith fn [0..(n-1)] cs
+presence n cs (DecorNegate d) = map not (presence n cs d)
 
 -- | Combine multiple decorations into one based on a selected function.
 combine
